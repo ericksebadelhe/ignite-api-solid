2eedd8f14f865d59f92b45adef423e6690ed8aa5
Erick Sebadelhe <ericksebadelhe@hotmail.com>
2023-10-10 19:10:44 -0600

    add remaining factories

---
+++ src/use-cases/factories/make-check-in-use-case.ts
@@ -0,0 +1,11 @@
+import { PrismaGymsRepository } from '@/repositories/prisma/prisma-gyms-repository'
+import { CheckInUseCase } from '../check-in'
+import { PrismaCheckInsRepository } from '@/repositories/prisma/prisma-check-ins-repository'
+
+export function makeCheckInUseCase() {
+  const checkInsRepository = new PrismaCheckInsRepository()
+  const gymsrepository = new PrismaGymsRepository()
+  const useCase = new CheckInUseCase(checkInsRepository, gymsrepository)
+
+  return useCase
+}
