204fedb51b7d4ca686765e4a1c2537ead167d6a4
Erick Sebadelhe <ericksebadelhe@hotmail.com>
2023-10-11 18:33:07 -0600

    finish chapter 6

---
+++ src/http/controllers/check-ins/metrics.spec.ts
@@ -0,0 +1,46 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { createAndAuthenticateUser } from '@/utils/create-and-authenticate-user'
+import { prisma } from '@/lib/prisma'
+
+describe('Check-in Metrics E2E', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to get the total count of check-ins', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const user = await prisma.user.findFirstOrThrow()
+
+    const gym = await prisma.gym.create({
+      data: {
+        title: 'Javascript Gym',
+        description: 'Some description.',
+        phone: '111111111111',
+        latitude: -27.0747279,
+        longitude: -49.4889672,
+      },
+    })
+
+    await prisma.checkIn.createMany({
+      data: [
+        { gym_id: gym.id, user_id: user.id },
+        { gym_id: gym.id, user_id: user.id },
+      ],
+    })
+
+    const response = await request(app.server)
+      .get('/check-ins/metrics')
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body.checkInsCount).toEqual(2)
+  })
+})
---
+++ src/http/controllers/gyms/nearby.spec.ts
@@ -0,0 +1,57 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { createAndAuthenticateUser } from '@/utils/create-and-authenticate-user'
+
+describe('Nearby Gyms E2E', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to list nearby gyms', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Near Gym',
+        description: 'near',
+        phone: '111111111111',
+        latitude: -27.0747279,
+        longitude: -49.4889672,
+      })
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Far Gym',
+        description: 'far',
+        phone: '222222222222',
+        latitude: -28.0610928,
+        longitude: -48.5229501,
+      })
+
+    const response = await request(app.server)
+      .get('/gyms/nearby')
+      .query({
+        latitude: -27.0747279,
+        longitude: -49.4889672,
+      })
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body.gyms).toHaveLength(1)
+    expect(response.body.gyms).toEqual([
+      expect.objectContaining({
+        title: 'Near Gym',
+      }),
+    ])
+  })
+})
